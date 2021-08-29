8260b90026bc693c44a2bd11b5c4899e01ac0740
ductilestudios <brian@ductilestudios.com>
2021-08-29 08:29:00 -0600

    revise listener as requested

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ app.use(function(err, req, res, next) {
   }  
 })
 
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log(`Node.js listening on port ${port}...`);
+//Listen on port set in environment variable or default to 3000
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Node.js listening on port" + listener.address().port);
 });
 
