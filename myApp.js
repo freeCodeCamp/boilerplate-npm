364a37d495081bb7d8b6d2b11f6292b38f685727
Kerlin <lopeskerlin@gmail.com>
2022-02-16 19:29:03 -0500

    env

--- myApp.js
+++ myApp.js
@@ -1,16 +1,45 @@
 var express = require('express');
 require('dotenv').config()
 var app = express();
+const dotenv = require("dotenv")
+dotenv.config()
 //console.log("Hello World")
 
-if (process.env.MESSAGE_STYLE == "uppercase") {
-  app.get("/json", function (req, res) {
-    res.json({ message: "HELLO JSON" });
-  });
-} else {
-  app.get("/json", function (req, res) {
-    res.json({ message: "Hello json" });
-  });
-}
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+
+app.get('/json',(req,res)=>{
+  if(process.env.MESSAGE_STYLE==="uppercase"){
+res.json({
+"message":"HELLO JSON"
+})
+  }else{
+    res.json({
+      "message":"Hello json"
+    })
+  }
+})
+
+// const mySecret = process.env['MESSAGE_STYLE'];
+// console.log(mySecret);
+
+// if (process.env.MESSAGE_STYLE == "uppercase") {
+//   app.get("/json", (req, res) => {
+//     res.json({ message: "HELLO JSON" });
+//   });
+// } else {
+//   app.get("/json", (req, res) => {
+//     res.json({ message: "Hello json" });
+//   });
+// }
 module.exports = app;
+
