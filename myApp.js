3c4fccad19773d010a6af7f2160f5ec4fab0e397
Nestor Ghenzi <46707985+nghenzi@users.noreply.github.com>
2022-04-24 06:55:02 +0900

    dotenv

--- myApp.js
+++ myApp.js
@@ -17,7 +17,7 @@ app.get("/", function(req, res) {
 
   app.get('/json', function(req, res) {
       if (process.env.MESSAGE_STYLE === 'uppercase') {
-        res.json({'message': 'Hello JSON'});}
+        res.json({'message': 'HELLO JSON'});}
         else {
             res.json({'message': 'Hello json'});
         }
