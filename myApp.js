1593a37e7f7d2a614f15f195cf78a204c613711c
Nestor Ghenzi <46707985+nghenzi@users.noreply.github.com>
2022-04-24 06:10:00 +0900

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public/style.css'));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
