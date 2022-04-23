9ef0be74da47f3bda1d5dd12ac875192c0009388
Nestor Ghenzi <46707985+nghenzi@users.noreply.github.com>
2022-04-24 06:12:33 +0900

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
