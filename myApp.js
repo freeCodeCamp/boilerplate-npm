d4cc547a0d3961a809166fe312c30b55017cffe2
Nestor Ghenzi <46707985+nghenzi@users.noreply.github.com>
2022-04-24 06:24:07 +0900

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -12,5 +12,9 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
 
+  app.get('/json', function(req, res) {
+        res.json({'message': 'Hello json'});
+    });
+
 
  module.exports = app;
