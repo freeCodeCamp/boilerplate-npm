4ddf50c187ee70a18a28be14dd0694b35bf9559f
Nestor Ghenzi <46707985+nghenzi@users.noreply.github.com>
2022-04-24 17:52:19 +0900

    asd

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)
-mongo db 
-0L9ZvGIwlFTt9yAb
-
-mongodb+srv://nes:0L9ZvGIwlFTt9yAb@cluster0.eudwv.mongodb.net/test?retryWrites=true
-
-
+# [MongoDB and Mongoose Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/mongodb-and-mongoose/)
--- myApp.js
+++ myApp.js
@@ -1,83 +1,74 @@
-var mongoose = require('mongoose');
-var express = require('express');
-var app = express();
-var dotenv = require('dotenv');
-var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
-
+var mongoose = require('mongoose');
 require('dotenv').config();
 
 const URI = process.env.MONGO_URI;
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const Schema = mongoose.Schema;
-const personSchema = new Schema({
-    name: { type: String, required: true },
-    age: Number,
-    favoriteFoods: [String]
-  });
+let Person;
 
-app.use(function middleware(req, res, next) {
-    // Do something
-    var string = req.method + " " + req.path + " - " + req.ip;
-    // Call the next function in line:
-    console.log(string);
-    next();
-  });
+const createAndSavePerson = (done) => {
+  done(null /*, data*/);
+};
 
-// Normal usage
-app.use(express.static(__dirname + "/public"));
+const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/);
+};
 
-// Assets at the /public route
-app.use("/public", express.static(__dirname + "/public"));
+const findPeopleByName = (personName, done) => {
+  done(null /*, data*/);
+};
 
+const findOneByFood = (food, done) => {
+  done(null /*, data*/);
+};
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-  });
+const findPersonById = (personId, done) => {
+  done(null /*, data*/);
+};
 
-app.get("/:word/echo", function(req, res) {
-    res.json({'echo': req.params.word});
-});
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
 
-app.route('/name')
-    .get((req,res)=>
-        res.json({name:req.query.first +
-             ' ' + req.query.last}))
+  done(null /*, data*/);
+};
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
 
-app.post("/name", function(req, res) {
-    // Handle the data in the request
-    const string = req.body.first + " " + req.body.last;
-    res.json({ name: string });
-});
+  done(null /*, data*/);
+};
 
+const removeById = (personId, done) => {
+  done(null /*, data*/);
+};
 
-app.get('/json', function(req, res) {
-    const message = {'message': 'Hello json'} 
-    if (process.env.MESSAGE_STYLE == 'uppercase') {
-        return res.json({'message': 'HELLO JSON'});
-    }
-    else {
-        return res.json(message);
-    }
-});
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
 
+  done(null /*, data*/);
+};
 
-app.get("/now",
-    (req, res, next) => {
-      req.time = new Date().toString();
-      next();
-    },
-    (req, res) => {
-      res.send({
-        time: req.time
-      });
-    }
-  );
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
 
+  done(null /*, data*/);
+};
 
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
 
- module.exports = app;
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
