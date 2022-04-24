24fd70fe56b36aa63abb60f333bce3e05e091dc0
Nestor Ghenzi <46707985+nghenzi@users.noreply.github.com>
2022-04-24 18:30:40 +0900

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -75,12 +75,16 @@ const findEditThenSave = (personId, done) => {
     })
   };
 
-const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
+  const findAndUpdate = (personName, done) => {
+    const ageToSet = 20;
+  
+    Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
+      if(err) return console.log(err);
+      done(null, updatedDoc);
+    })
+  };
 
-  done(null /*, data*/);
-};
-
+  
 const removeById = (personId, done) => {
   done(null /*, data*/);
 };
