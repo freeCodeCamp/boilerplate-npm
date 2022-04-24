1b7ace02b3ba3b287d01ab1bbe3d58d9ec843641
Nestor Ghenzi <46707985+nghenzi@users.noreply.github.com>
2022-04-24 18:28:31 +0900

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -58,10 +58,22 @@ var findPersonById = function(personId, done) {
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
-};
+    const foodToAdd = 'hamburger';
+  
+    // .findById() method to find a person by _id with the parameter personId as search key. 
+    Person.findById(personId, (err, person) => {
+      if(err) return console.log(err); 
+    
+      // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+      person.favoriteFoods.push(foodToAdd);
+  
+      // and inside the find callback - save() the updated Person.
+      person.save((err, updatedPerson) => {
+        if(err) return console.log(err);
+        done(null, updatedPerson)
+      })
+    })
+  };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
