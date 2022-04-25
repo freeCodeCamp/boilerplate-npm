0cc5ca2ab6009975880d3c6289e3b24480e76ed0
Nestor Ghenzi <46707985+nghenzi@users.noreply.github.com>
2022-04-25 12:44:32 +0900

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -75,8 +75,8 @@ suite('Unit Tests', function () {
     });
     // #10
     test('#approximately', function () {
-      assert.aproximately(weirdNumbers(0.5), 1, 0.5);
-      assert.aproximately(weirdNumbers(0.2), 1, 0.8);
+      assert.approximately(weirdNumbers(0.5), 1, 0.5);
+      assert.approximately(weirdNumbers(0.2), 1, 0.8);
     });
   });
 
